a704cc402ce9eba80ec1751b8512ee4a1667d625
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-27 11:17:42 +0200

    Create app.js

---
+++ Module_II/Lab/app.js
@@ -0,0 +1,11 @@
+let maxwell = ["Maxwell Wright", "(0191) 719 6495", "Curabitur.egestas.nunc@nonummyac.co.uk"];
+let raja = ["Raja Villarreal", "0866 398 2895", "posuere.vulputate@sed.com"];
+let helen = ["Helen Richards	", "0800 1111", "	libero@convallis.edu"];
+
+for (let i = 0; i < maxwell.length; i++) console.log(maxwell[i]);
+console.log("\n");
+
+for (let i = 0; i < maxwell.length; i++) console.log(raja[i]);
+console.log("\n");
+
+for (let i = 0; i < maxwell.length; i++) console.log(helen[i]);
