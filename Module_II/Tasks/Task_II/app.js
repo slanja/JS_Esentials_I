77c34405ace5b7dbc22216deea4e5d99909ac08b
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-27 11:34:39 +0200

    Create app.js

---
+++ Module_II/Tasks/Task_II/app.js
@@ -0,0 +1,14 @@
+let b1 = true;
+let b2 = Boolean(true);
+
+let n1 = 3;
+let n2 = Number(24);
+
+let bi1 = 24n;
+let bi2 = BigInt(24);
+
+let s1 = "slanja";
+let s2 = String("slanja");
+
+console.log(`${typeof s1}: ${s1}`);
+console.log(`${typeof n1}: ${n1}`);
