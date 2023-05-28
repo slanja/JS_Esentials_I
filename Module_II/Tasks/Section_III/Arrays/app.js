0deefb987b3dc9aafe3c2e6cd82a13408c0ee406
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-28 08:23:33 +0200

    Create app.js

---
+++ Module_II/Tasks/Section_III/Arrays/app.js
@@ -0,0 +1,21 @@
+var books = {
+    [
+        title: "Speaking JavaScript",
+        author: "Axel Rauschmayer",
+        pages: 460,
+    ],
+    [
+        title: "Programming JavaScript Applications",
+        author: "Eric Elliott",
+        pages: 254,
+    ],
+    [
+        title: "Understanding ECMAScript 6",
+        author: "Nicholas C. Zakas",
+        pages: 352,    
+    ]};
+    
+    // adding new book
+    // title: "Learning JavaScript Design Patterns",
+    // author: "Addy Osmani",
+    // pages: 254,
