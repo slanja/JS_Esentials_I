add610f0e77ddfc72d9e17798fc4c34d395ce754
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-31 16:58:26 +0200

    Create app.js

---
+++ Module_II/Tasks/Section_III/Arrays/Task_V/app.js
@@ -0,0 +1,32 @@
+let books = [
+    {
+        title: "Speaking JavaScript",
+        author: "Axel Rauschmayer",
+        pages: 460,
+    },
+    {
+        title: "Programming JavaScript Applications",
+        author: "Eric Elliott",
+        pages: 254,
+    },
+    {
+        title: "Understanding ECMAScript 6",
+        author: "Nicholas C. Zakas",
+        pages: 352   
+    }];
+    
+    // adding new book
+let newBook = {
+        title: "Learning JavaScript Design Patterns",
+        author: "Addy Osmani",
+        pages: 254
+    };
+    
+books.push(newBook);
+
+// selecting last two books
+let copiedBooks = books.slice(-2);
+    
+for (let i = 0; i < books.length; i++) {   
+    console.log("Title: " + books[i].title + " Author: " + books[i].author + " Pages: " + books[i].pages);
+}
