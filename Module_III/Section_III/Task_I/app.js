98d5c494b485669750227f10db4b9c13ec14cd49
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-06-04 22:20:41 +0200

    Create app.js

---
+++ Module_III/Section_III/Task_I/app.js
@@ -0,0 +1,10 @@
+// getting values
+a = window.prompt("Width:");
+b = prompt("Height:");
+c = prompt("Length:");
+
+// formula for volume
+result = a * b * c;
+
+// printing result using alert
+window.alert("Volume: " + result);
